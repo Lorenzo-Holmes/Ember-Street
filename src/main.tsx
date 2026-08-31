13e1259e448e66d3c59460e0c9dde25a4cdfed60
Lorenzo Holmes <125287880+Lorenzo-Holmes@users.noreply.github.com>
2026-08-31 23:15:51 +0800

    fix: route app through v0.6 interaction hotfix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import V060App from './V060App';
+import V060AppHotfix from './V060AppHotfix';
 import './v060.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <V060App />
+    <V060AppHotfix />
   </React.StrictMode>,
 );
